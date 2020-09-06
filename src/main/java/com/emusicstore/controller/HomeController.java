8526ae1bf35f15b0519754f6937d9f08d9b8556b
Dongyu Li <dongyuli511@gmail.com>
2020-09-05 19:28:35 -0700

    add viewProduct page

--- src/main/java/com/emusicstore/controller/HomeController.java
+++ src/main/java/com/emusicstore/controller/HomeController.java
@@ -31,4 +31,10 @@ public class HomeController {
         return "productList";
 
     }
+
+    @RequestMapping("/productList/viewProduct")
+    public String viewProduct() {
+        return "viewProduct";
+    }
+
 }
